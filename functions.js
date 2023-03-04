fd83ea12f8a4c64c47d98741cd7c1f37064a22e6
CalebWelch <caleb.d.welch@att.net>
2023-03-03 23:51:25 -0600

    got timer working with touchdesigner

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 var uri = "wss://game-test-td.herokuapp.com:443";
 const socket = new WebSocket("wss://game-test-td.herokuapp.com:443");
 const timer = 3;
+
 window.addEventListener("load", () => {
     socket.addEventListener("open", () => {
         socket.send("test");
@@ -8,17 +9,8 @@ window.addEventListener("load", () => {
     });
     document.getElementById("submit").disabled = true;
 });
-async function connect() {}
-function test() {
-    const msg = {
-        type: "message",
-        name: document.getElementById("name").value,
-        date: Date.now(),
-    };
-    socket.send(JSON.stringify(msg));
-    var name = document.getElementById("name").value;
-    console.log(name);
-}
+
+var preclick = false;
 socket.addEventListener("message", (event) => {
     data = JSON.parse(event.data);
     var button = document.getElementById("submit");
@@ -26,11 +18,26 @@ socket.addEventListener("message", (event) => {
         button.disabled = false;
         button.style.backgroundColor = "rgb(246, 249, 250)";
     } else if (data.timer >= 0) {
+        button.onclick = () => {
+            button.style.backgroundColor = "red";
+            button.disabled = true;
+            preclick = true;
+            console.log("preclick", preclick);
+        };
         document.getElementById("timer").innerText = data.timer;
-        if (data.timer == 0) button.style.backgroundColor = "green";
+        if (data.timer == 0) {
+            button.onclick = function () {
+                const msg = {
+                    type: "message",
+                    name: document.getElementById("name").value,
+                    date: Date.now(),
+                };
+                socket.send(JSON.stringify(msg));
+                var name = document.getElementById("name").value;
+                console.log(name);
+                button.disabled = true;
+            };
+            if (preclick == false) button.style.backgroundColor = "green";
+        }
     }
-    button.onclick = function () {
-        alert("button was clicked");
-        button.disabled = true;
-    };
 });
