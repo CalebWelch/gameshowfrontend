d0c3be204877690a88bd76dd7e57064c3b8f30cb
CalebWelch <caleb.d.welch@att.net>
2023-03-03 22:25:35 -0600

    adding button functionality

--- functions.js
+++ functions.js
@@ -1,19 +1,36 @@
 var uri = "wss://game-test-td.herokuapp.com:443";
 const socket = new WebSocket("wss://game-test-td.herokuapp.com:443");
+const timer = 3;
 window.addEventListener("load", () => {
     socket.addEventListener("open", () => {
         socket.send("test");
-        console.log("done");
+        alert("done");
     });
+    document.getElementById("submit").disabled = true;
 });
 async function connect() {}
 function test() {
     const msg = {
         type: "message",
-        text: document.getElementById("name").value,
+        name: document.getElementById("name").value,
         date: Date.now(),
     };
     socket.send(JSON.stringify(msg));
     var name = document.getElementById("name").value;
     console.log(name);
 }
+socket.addEventListener("message", (event) => {
+    data = JSON.parse(event.data);
+    var button = document.getElementById("submit");
+    if (data.prime) {
+        button.disabled = false;
+        button.style.backgroundColor = "rgb(246, 249, 250)";
+    } else if (data.timer >= 0) {
+        document.getElementById("timer").innerText = data.timer;
+        if (data.timer == 0) button.style.backgroundColor = "green";
+    }
+    button.onclick = function () {
+        alert("button was clicked");
+        button.disabled = true;
+    };
+});
